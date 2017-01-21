2e6fb369c702e75ce19eb3368c0d11e3423a9dca
AnujDasari <anuj.dasari@gmail.com>
2017-01-21 13:19:44 +0530

    Updated App script and Test Data sheets.
    Updated App script and Test Data sheets.

--- src/test/java/com/actitime/testscripts/TestScriptsApp.java
+++ src/test/java/com/actitime/testscripts/TestScriptsApp.java
@@ -10,23 +10,9 @@ import com.actitime.genericlibrary.FileUtility;
  */
 public class TestScriptsApp extends SuperReference {
 	@Test
-	public void TC01_SignIntoApplication() {
-		FileUtility.retrieveData("TC01_SignIntoApplication");
-		loginPO.login();
+	public void TC01_SignIntoApp() {
+		
 	}
 
-	@Test
-	public void TC02_NavigateToTasks() {
-		dashBoardPO.navigateToTasks();
-	}
-
-	@Test
-	public void TC03_NavigateToReports() {
-		dashBoardPO.navigateToReports();
-	}
-
-	@Test
-	public void TC04_NavigateToUsers() {
-		dashBoardPO.navigateToUsers();
-	}
+	
 }
